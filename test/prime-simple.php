ebd6ee6fb6e2d47f285a4690abea8692e4a7af22
lisces <lisces@gmail.com>
2013-12-17 11:01:08 +0800

    prime number simple

---
+++ test/prime-simple.php
@@ -0,0 +1,19 @@
+<?php
+$n = isset($argv[1]) ? $argv[1] : 0;
+if($n < 2){
+  return false;
+}
+for($i=2;$i<=$n;$i++){
+  $a[$i] = $i;
+}
+for($i=2;$i<=$n;$i++){
+  for($j=2;;$j++){
+    $d = $i * $j;
+    if($d <= $n){
+      unset($a[$d]);
+    } else {
+      break;
+    }
+  }
+}
+print_r($a);
--- test/prime.php
+++
@@ -1,8 +0,0 @@
-<?php
-$n = isset($argv[1]) ? $argv[1] : 0;
-if($n < 1){
-  return false;
-}
-for($i=2;;$i++){
-
-}
